c7f9f276e379a0534dd66a5577c5f8846426cd62
deenanath <deenanathmahato.cse@gmail.com>
2020-07-21 21:45:19 +0530

    getCountryData method implementation done

--- templates/basic website/js/script.js
+++ templates/basic website/js/script.js
@@ -1,9 +1,26 @@
+let URL ="https://covid19.mathdro.id/api";
+
 let app = angular.module ("myApp", []);
 app.controller ("myCtroller", ($scope, $http) => {
-    $http.get ("https://covid19.mathdro.id/api").then ((response) => {
-        console.log (response);
+    $scope.title = "Stay Home Stay Safe!!"
+
+    $http.get (URL).then ((response) => {
         $scope.allDatas = response.data;
     }, (error) => {
         console.log (error);
     })
+
+    $scope.getContryData = () => {
+        let country = $scope.countryName;
+        if (country == "") {
+            $scope.country_datas = undefined;
+        } 
+
+        $http.get (URL + "/countries/" + country).then ((resp) => {
+            $scope.country_datas = resp.data;
+        }, (error) => {
+            console.log (error);
+            $scope.country_datas = undefined;
+        })
+    }
 });
